47d2e2b86c14711f0b8555d6b5aaaa904c13803e
Glory-Lyka Okposin <lykaedem11@gmail.com>
2024-01-31 20:37:47 +0100

    modified

--- index.js
+++ index.js
@@ -2,20 +2,22 @@ const http = require("http");
 const PORT = 8900;
 
 const server = http.createServer((req, res) => {
-  console.log(req.method);
+  console.log(process.version);
   console.log(req.url);
-
-  switch (req.url) {
-    case "/":
-      res.write("Hi, I'm Glory Edem a.k.a 'LYKA'");
-      break;
-    default:
-      res.write("404 error, the url request doesn't exist.");
-      break;
+  console.log(req.method);
+  if (req.url === "/") {
+    res.write(`Hi, hi Team !!!`);
+    res.end();
+  } else if (req.url === "/hello" && req.method === "POST") {
+    res.write(
+      `My name is Glory Okposin Edem. I'm am Altschool Backend Engineering student.`
+    );
+    res.end();
+  } else {
+    res.write(`404 ERROR`);
+    res.end();
   }
-  res.end("");
 });
-
 server.listen(PORT, null, null, () => {
   console.log("Sever is listening at port: 8900");
 });
