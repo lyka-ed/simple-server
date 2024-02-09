57b123874db46c52e35fe218c4315eb272122657
Glory-Lyka Okposin <lykaedem11@gmail.com>
2024-02-09 01:10:25 +0100

    modified

--- index.js
+++ index.js
@@ -3,20 +3,34 @@ const http = require("http");
 const PORT = 8900;
 
 const server = http.createServer((req, res) => {
-  console.log(req.method);
-  if (req.url === "/") {
-    res.write(`Hi, hi Team !!!`);
-    res.end();
-  } else if (req.url === "/name" && req.method === "POST") {
-    res.write(
-      `My name is Glory Okposin Edem. I'm am Altschool Backend Engineering student.`
-    );
-    res.end();
-  } else {
-    res.write(`404 ERROR`);
-    res.end();
+  switch (req.url) {
+    case "/":
+      res.write(`Hi, hi Team !!!`);
+      break;
+    case "/name":
+      res.write(
+        `My name is Glory Okposin Edem. I'm am Altschool Backend Engineering student.`
+      );
+      break;
+    default:
+      res.statusCode = 404;
+      break;
   }
+  res.end();
 });
+//   if (req.url === "/") {
+//     res.write(`Hi, hi Team !!!`);
+//     res.end();
+//   } else if (req.url === "/name" && req.method === "POST") {
+//     res.write(
+//       `My name is Glory Okposin Edem. I'm am Altschool Backend Engineering student.`
+//     );
+//     res.end();
+//   } else {
+//     res.write(`404 ERROR`);
+//     res.end();
+//   }
+// });
 server.listen(PORT, () => {
   console.log(`ever is listening at port ${PORT}`);
 });
